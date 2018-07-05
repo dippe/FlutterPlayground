cf1822b3b8f8001dda0716364337e469209169fd
Peter Dajka <dippede@gmail.com>
2018-07-05 14:21:15 +0200

    further implementation of immutable state: equals, data modifications, unit tests

--- lib/examples/state/immutable/TodoState.dart
+++ lib/examples/state/immutable/TodoState.dart
@@ -11,35 +11,89 @@ class TodoData {
   const TodoData(this.id, this.title, this.done);
 
   @override
-  bool operator ==(other) => id == (other.id) && title == other.title && done == other.done;
+  bool operator ==(other) =>
+      other is TodoData && id == (other.id) && title == other.title && done == other.done;
+
+  TodoData withId(int id) {
+    return TodoData(id, title, done);
+  }
 }
 
-class TodoItems {
+class Todos {
   final Map<int, TodoData> items;
   final int idCounter;
 
-  TodoItems({this.items, this.idCounter});
+  Todos({this.items, this.idCounter});
 
-// fixme: implement ==
+  Todos withNewItem(TodoData d) {
+    Map<int, TodoData> copy = Map.from(items);
+    var nextId = idCounter + 1;
+    copy.putIfAbsent(nextId, () => d.withId(nextId));
+    return new Todos(items: copy, idCounter: nextId);
+  }
+
+  @override
+  bool operator ==(other) =>
+      other is Todos &&
+      other.items != null &&
+      _itemsEqual(items, other.items) &&
+      idCounter == other.idCounter;
+
+  bool _itemsEqual(Map items, Map otherItems) {
+    bool isEq = true;
+    items.forEach((key, i) {
+      if (otherItems[key] != items[key]) {
+        isEq = false;
+      }
+    });
+    return otherItems is Map<int, TodoData> && isEq;
+  }
+}
+
+class TodoListView {
+  final int selectedTitleForEdit;
+  final int selectedItem;
+
+  TodoListView({this.selectedTitleForEdit, this.selectedItem});
+
+  @override
+  bool operator ==(other) =>
+      other is TodoListView &&
+      selectedItem == other.selectedItem &&
+      selectedTitleForEdit == other.selectedTitleForEdit;
 }
 
 class TodoState {
-  final int selectedTitleForEdit;
-  final int selectedItem;
-  final TodoItems todos;
+  final Todos todos;
+  final TodoListView listView;
 
-  TodoState({this.selectedTitleForEdit, this.selectedItem, this.todos});
+  TodoState({this.listView, this.todos});
 
-// fixme: implement ==
+  TodoState withTodos(newElem) => new TodoState(listView: listView, todos: newElem);
+  TodoState withListView(newElem) => new TodoState(listView: newElem, todos: todos);
 
-//  TodoState changeTitleState(TitleState titleState) => new AppState(titleState: titleState);
+  @override
+  bool operator ==(other) =>
+      other is TodoState && todos == other.todos && listView == other.listView;
 }
 
-var state = new Immutable<TodoState>(new TodoState(
-  todos: new TodoItems(
+var appState = new Immutable<TodoState>(new TodoState(
+  todos: new Todos(
     items: new Map.unmodifiable({0: new TodoData(0, 'Hello world :P', false)}),
     idCounter: 1,
   ),
-  selectedItem: 2,
-  selectedTitleForEdit: 3,
+  listView: new TodoListView(
+    selectedItem: 2,
+    selectedTitleForEdit: 3,
+  ),
 ));
+
+var todosState = appState.property(
+  (c) => c.todos,
+  change: (state, newTodos) => state.withTodos(newTodos),
+);
+
+var listViewState = appState.property(
+  (c) => c.listView,
+  change: (state, newElem) => state.withTodos(newElem),
+);
--- test/examples/state/immutable/TodoState_test.dart
+++ test/examples/state/immutable/TodoState_test.dart
@@ -10,11 +10,65 @@ void main() {
     expect(undertest == undertest2, isTrue);
     expect(identical(undertest, undertest2), isFalse);
   });
+  test('State is immutable', () {
+    var origState = appState.current;
 
-  test('State is immutable', () async {
-    var undertest = state;
+    todosState.change((old) => Todos(idCounter: old.idCounter, items: old.items));
+    todosState.change((old) {
+      // add a new item
+      Map<int, TodoData> items = Map.from(old.items);
+      items.putIfAbsent(2, () => TodoData(2, 't2', false));
+      return Todos(
+        idCounter: old.idCounter,
+        items: items,
+      );
+    });
 
-//    expect(state. == undertest2, isTrue);
-//    expect(identical(undertest, undertest2), isFalse);
+    var expectedState = origState.withTodos(origState.todos.withNewItem(TodoData(2, 't2', false)));
+
+    expect(appState.onChange, emitsAnyOf([origState, expectedState]));
+  });
+
+  test('Todo(s) with same content are equal', () async {
+    var todo1 = TodoData(1, 'title', true);
+    var todo2 = TodoData(1, 'title', true);
+
+    expect(todo1 == todo2, isTrue);
+  });
+
+  test('Todo(s) with different content are not equal', () async {
+    var todo1 = TodoData(1, 'title', true);
+    var todo2 = TodoData(1, 'title', true);
+
+    expect(TodoData(2, 'title', true) == TodoData(1, 'title', true), isFalse);
+    expect(TodoData(1, 'title1', true) == TodoData(1, 'title', true), isFalse);
+    expect(TodoData(1, 'title', false) == TodoData(1, 'title', true), isFalse);
+  });
+
+  test('Todos items equality is OK', () async {
+    var todo1 = TodoData(1, 'title', true);
+    var todo2 = TodoData(2, 'title2', false);
+
+    var list0 = Todos(idCounter: 0, items: {1: todo1, 2: todo2});
+    var list1 = Todos(idCounter: 0, items: {1: todo1, 2: todo2});
+    var list2 = Todos(idCounter: 0, items: {3: todo1, 2: todo2});
+    var list3 = Todos(idCounter: 0, items: {1: null, 2: todo2});
+    var list4 = Todos(idCounter: 0, items: null);
+
+    expect(list1 == list0, isTrue);
+    expect(list1 == list1, isTrue);
+    expect(list1 == list2, isFalse);
+    expect(list1 == list3, isFalse);
+    expect(list1 == list4, isFalse);
+  });
+
+  test('Todos withNewItem adds a new element', () async {
+    var todo1 = TodoData(1, 'title', true);
+    var todo2 = TodoData(2, 'title2', false);
+
+    var underTest = Todos(idCounter: 1, items: {1: todo1}).withNewItem(todo2);
+    var expectedList = Todos(idCounter: 2, items: {1: todo1, 2: todo2});
+
+    expect(underTest == expectedList, isTrue);
   });
 }

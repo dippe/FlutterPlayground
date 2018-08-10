d34854291bcf65749c343d9eb1a548e2a64fbff7
Peter Dajka <dippede@gmail.com>
2018-08-10 17:10:46 +0200

    add multiple predefined filters for later usage

--- lib/state/state.dart
+++ lib/state/state.dart
@@ -16,10 +16,35 @@ final _initState = TodoAppState(
   login: LoginData(TMP_USER, TMP_PWD),
   fetchedIssues: null,
   view: ViewData(
-    selectedIssueListView: '1',
+    selectedIssueListView: '2',
     actual: PageType.IssueList,
     issueListViews: {
-      '1': IssueListView(id: '1', filter: JiraFilter(id: '1', jql: 'project=test', name: '1FilterName')),
+      '1': IssueListView(id: '1', filter: JiraFilter(id: '1', jql: 'project=test', name: '1 Project Test')),
+      '2': IssueListView(id: '2', filter: JiraFilter(id: '2', jql: 'status in ("To Do")', name: '2 Status Todo')),
+      // Usable predefined filters:
+      '3': IssueListView(
+        id: '3',
+        filter: JiraFilter(id: '3', jql: 'resolutiondate >= -1w order by updated DESC', name: 'Resolved recently'),
+      ),
+      '4': IssueListView(
+        id: '4',
+        filter: JiraFilter(id: '4', jql: 'updated >= -1w order by updated DESC', name: 'Updated recently'),
+      ),
+      '5': IssueListView(
+        id: '5',
+        filter: JiraFilter(id: '5', jql: 'created >= -1w order by created DESC', name: 'Created recently'),
+      ),
+      '6': IssueListView(
+        id: '6',
+        filter: JiraFilter(id: '6', jql: 'reporter = currentUser() order by created DESC', name: 'Reported by me'),
+      ),
+      '7': IssueListView(
+          id: '7',
+          filter: JiraFilter(
+              id: '7',
+              jql: 'assignee = currentUser() AND resolution = Unresolved order by updated '
+                  'DESC',
+              name: 'My open issues')),
     },
   ),
 );

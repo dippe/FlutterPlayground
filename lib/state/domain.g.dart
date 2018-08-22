b6526ed219b2c9c9d33740d5306f5cba0f58b885
Peter Dajka <dippede@gmail.com>
2018-08-22 11:06:33 +0200

    fix domain.g dart warnings (required fields)

--- lib/state/domain.g.dart
+++ lib/state/domain.g.dart
@@ -8,16 +8,15 @@ part of 'domain.dart';
 
 IssueListView _$IssueListViewFromJson(Map<String, dynamic> json) {
   return IssueListView(
-      id: json['id'] as String,
-      name: json['name'] as String,
-      filter: json['filter'] == null
-          ? null
-          : JiraFilter.fromJson(json['filter'] as Map<String, dynamic>),
-      idCounter: json['idCounter'] as int);
+    id: json['id'] as String,
+    name: json['name'] as String,
+    filter: json['filter'] == null ? null : JiraFilter.fromJson(json['filter'] as Map<String, dynamic>),
+    idCounter: json['idCounter'] as int,
+    items: <ListItemData>[],
+  );
 }
 
-Map<String, dynamic> _$IssueListViewToJson(IssueListView instance) =>
-    <String, dynamic>{
+Map<String, dynamic> _$IssueListViewToJson(IssueListView instance) => <String, dynamic>{
       'id': instance.id,
       'name': instance.name,
       'filter': instance.filter,
@@ -25,9 +24,7 @@ Map<String, dynamic> _$IssueListViewToJson(IssueListView instance) =>
     };
 
 SearchState _$SearchStateFromJson(Map<String, dynamic> json) {
-  return SearchState(
-      recent: (json['recent'] as List)?.map((e) => e as String)?.toList(),
-      text: json['text'] as String);
+  return SearchState(recent: (json['recent'] as List)?.map((e) => e as String)?.toList(), text: json['text'] as String);
 }
 
 Map<String, dynamic> _$SearchStateToJson(SearchState instance) =>
@@ -35,16 +32,14 @@ Map<String, dynamic> _$SearchStateToJson(SearchState instance) =>
 
 ViewState _$ViewStateFromJson(Map<String, dynamic> json) {
   return ViewState(
-      actPage: _$enumDecodeNullable(_$PageTypeEnumMap, json['actPage']),
-      issueListViews: (json['issueListViews'] as List)
-          ?.map((e) => e == null
-              ? null
-              : IssueListView.fromJson(e as Map<String, dynamic>))
-          ?.toList(),
-      actListIdx: json['actListIdx'] as int,
-      search: json['search'] == null
-          ? null
-          : SearchState.fromJson(json['search'] as Map<String, dynamic>));
+    actPage: _$enumDecodeNullable(_$PageTypeEnumMap, json['actPage']),
+    issueListViews: (json['issueListViews'] as List)
+        ?.map((e) => e == null ? null : IssueListView.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+    actListIdx: json['actListIdx'] as int,
+    search: json['search'] == null ? null : SearchState.fromJson(json['search'] as Map<String, dynamic>),
+    messages: null,
+  );
 }
 
 Map<String, dynamic> _$ViewStateToJson(ViewState instance) => <String, dynamic>{
@@ -61,8 +56,7 @@ T _$enumDecode<T>(Map<T, dynamic> enumValues, dynamic source) {
   }
   return enumValues.entries
       .singleWhere((e) => e.value == source,
-          orElse: () => throw ArgumentError(
-              '`$source` is not one of the supported values: '
+          orElse: () => throw ArgumentError('`$source` is not one of the supported values: '
               '${enumValues.values.join(', ')}'))
       .key;
 }
@@ -83,12 +77,10 @@ const _$PageTypeEnumMap = <PageType, dynamic>{
 
 AppState _$AppStateFromJson(Map<String, dynamic> json) {
   return AppState(
-      config: json['config'] == null
-          ? null
-          : ConfigState.fromJson(json['config'] as Map<String, dynamic>),
-      view: json['view'] == null
-          ? null
-          : ViewState.fromJson(json['view'] as Map<String, dynamic>));
+    config: json['config'] == null ? null : ConfigState.fromJson(json['config'] as Map<String, dynamic>),
+    view: json['view'] == null ? null : ViewState.fromJson(json['view'] as Map<String, dynamic>),
+    jira: null,
+  );
 }
 
 Map<String, dynamic> _$AppStateToJson(AppState instance) =>
@@ -99,19 +91,14 @@ ConfigState _$ConfigStateFromJson(Map<String, dynamic> json) {
       user: json['user'] as String,
       password: json['password'] as String,
       baseUrl: json['baseUrl'] as String,
-      listViewMode:
-          _$enumDecodeNullable(_$ListViewModeEnumMap, json['listViewMode']));
+      listViewMode: _$enumDecodeNullable(_$ListViewModeEnumMap, json['listViewMode']));
 }
 
-Map<String, dynamic> _$ConfigStateToJson(ConfigState instance) =>
-    <String, dynamic>{
+Map<String, dynamic> _$ConfigStateToJson(ConfigState instance) => <String, dynamic>{
       'user': instance.user,
       'password': instance.password,
       'baseUrl': instance.baseUrl,
       'listViewMode': _$ListViewModeEnumMap[instance.listViewMode]
     };
 
-const _$ListViewModeEnumMap = <ListViewMode, dynamic>{
-  ListViewMode.NORMAL: 'NORMAL',
-  ListViewMode.COMPACT: 'COMPACT'
-};
+const _$ListViewModeEnumMap = <ListViewMode, dynamic>{ListViewMode.NORMAL: 'NORMAL', ListViewMode.COMPACT: 'COMPACT'};

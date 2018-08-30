148b3920e71ebcc405dc138a72ff6e2affb147fa
Peter Dajka <dippede@gmail.com>
2018-08-30 12:41:28 +0200

    UX: add scrollbar to the issue lists

--- lib/view/issue_list/issue_list.dart
+++ lib/view/issue_list/issue_list.dart
@@ -4,14 +4,16 @@ import 'package:todo_flutter_app/view/issue_list/item/list_item.dart';
 
 // the progress indicator cannot be rendered outside because the flutter crashes when adding a new column / etc
 // wrapper to this
-Widget wIssueList(List<ListItemData> issues, bool showProgressIndicator, bool isCompact) => ListView(
-      scrollDirection: Axis.vertical,
-      children: issues.map((item) => wDraggableListItem(item, isCompact)).toList()
-        ..insert(
-          0,
-          new SizedBox(
-            height: 3.0,
-            child: showProgressIndicator ? new LinearProgressIndicator() : null,
+Widget wIssueList(List<ListItemData> issues, bool showProgressIndicator, bool isCompact) => Scrollbar(
+      child: ListView(
+        scrollDirection: Axis.vertical,
+        children: issues.map((item) => wDraggableListItem(item, isCompact)).toList()
+          ..insert(
+            0,
+            new SizedBox(
+              height: 3.0,
+              child: showProgressIndicator ? new LinearProgressIndicator() : null,
+            ),
           ),
-        ),
+      ),
     );

e83d13d168b5f6c4cfd625ae872e4308cd353843
Peter Dajka <dippede@gmail.com>
2018-08-31 14:54:06 +0200

    Revert "Refactor/fix: duplicated password reset on decoding error. Remove uneeded Action call: setPwd"
    This reverts commit 6177491
    
    Reason: infinite loop without this

--- lib/view/config/reducer.dart
+++ lib/view/config/reducer.dart
@@ -47,6 +47,8 @@ getDecryptedPwd(ConfigState state, pwd) {
     final decrypted = blockCipher.decodeB64(pwd);
     return decrypted;
   } catch (e) {
+    // FIXME: ?
+    store.dispatch(Actions.SetPwd(''));
     store.dispatch(AddWarningMessageAction('Password decrypt error: try to re-enter the pwd!  ' + e.toString()));
     return '';
   }
